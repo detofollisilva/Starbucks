f96b56702086a4f433dc8f798268de609870a12d
detofollisilva <fabim.cajobi@hotmail.com>
2025-10-13 16:41:19 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
 let imagem = document.querySelector(".imagem-copo");
 let fundo = document.querySelector(".circulo");
 
-function trocaImagem(endereco) {	
+function trocaImagem(endereco) {
     imagem.src = endereco;
 }
 
 function trocaFundo(cor) {
     fundo.style.background = cor;
-}   
+}
+
+// ====== EFEITO DE TRANSIÇÃO ENTRE PÁGINAS ======
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.remove("fade-out");
+});
+
+function mudarPaginaComTransicao(url) {
+    document.body.classList.add("fade-out");
+    setTimeout(() => {
+        window.location.href = url;
+    }, 500); // tempo igual ao transition no CSS
+}
